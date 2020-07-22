e3c31f0af03b0ac469f190330ad207f6128cb8e9
Dave <alexruedarivas@outlook.com>
2020-07-22 14:12:28 -0500

    Updates

---
+++ precache-manifest.287a14cbe9ac7a7fdfa8c763704fc5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99926d3a0ef5f889eb7c54402050193c",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "2e6664ab35e32e5a9a76",
+    "url": "/todo-list/static/css/main.2799f600.chunk.css"
+  },
+  {
+    "revision": "1632fae21d3ddd9688cd",
+    "url": "/todo-list/static/js/2.ead4bc06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.ead4bc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6664ab35e32e5a9a76",
+    "url": "/todo-list/static/js/main.8084ddca.chunk.js"
+  },
+  {
+    "revision": "76252ccba03636db080b",
+    "url": "/todo-list/static/js/runtime-main.b228cddc.js"
+  }
+]);
